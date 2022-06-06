72ed60a6cfa13e6107447d3bdfae07b0c717e903
Felix Leitl <felix.leitl@icloud.com>
2022-06-07 00:25:26 +0200

    Added the left images

--- You are awsome!/ViewController.swift
+++ You are awsome!/ViewController.swift
@@ -8,20 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
     
+    var imageNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         messageLabel.text = ""
     }
-
+    
     @IBAction func messageButtonPressed(_ sender: UIButton) {
-        messageLabel.text = "You are Awsome"
-        imageView.image = UIImage.init(named: "image0")
+                
+        print(imageNumber)
+        //let imageName = "image" + String(imageNumber)
+        let imageName = "image\(imageNumber)"
+        imageView.image = UIImage(named: imageName)
+        if imageNumber == 9 {
+            imageNumber = 0
+        } else {
+            imageNumber = imageNumber + 1
+        }
+       
+       
+        
+                //        let awsomeMessage = "You Are Awsome!"
+                //        let greatMessage = "You Are Great!"
+                //
+                //        if messageLabel.text == awsomeMessage {
+                //            messageLabel.text = greatMessage
+                //            imageView.image = UIImage (named: "image1")
+                //        } else {
+                //            messageLabel.text = awsomeMessage
+                //            imageView.image = UIImage.init(named: "image0")
+                //        }
     }
     
 }
