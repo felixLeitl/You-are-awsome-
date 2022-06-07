797e622502c1ec897eb22736a3f5261b8f2aac91
Felix Leitl <felix.leitl@icloud.com>
2022-06-07 22:14:31 +0200

    Added new Messages

--- You are awsome!/ViewController.swift
+++ You are awsome!/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     
     var imageNumber = 0
+    var messageNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,15 +23,28 @@ class ViewController: UIViewController {
     
     @IBAction func messageButtonPressed(_ sender: UIButton) {
         
-        print(imageNumber)
         //let imageName = "image" + String(imageNumber)
         let imageName = "image\(imageNumber)"
+        let messages = ["You Are Awsome", "You Are Great!", "You Are A Legend!", "You Are Da Bomb!"]
+        
+        print(imageNumber)
+        print(messageNumber)
+        
         imageView.image = UIImage(named: imageName)
         
         if imageNumber == 9 {
             imageNumber = 0
         } else {
             imageNumber = imageNumber + 1
+        }
+        
+        if messageNumber == messages.count {
+            messageNumber = 0
+            messageLabel.text = messages[0]
+            messageNumber += 1
+        }else{
+            messageLabel.text = messages[messageNumber]
+            messageNumber += 1
         }
     }
     
